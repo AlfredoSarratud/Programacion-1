95315cdbd082ed9f97001288d8f4f28e95dcfd44
Alfredo Sarratud <78238434+AlfredoSarratud@users.noreply.github.com>
2021-02-04 17:45:14 -0400

    Ejercicio practico primer corte Alfredo Sarratud
    programa que ordena tres números (abc) de manera aacb, bca y bcbc

---
+++ Programa 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Programa_1
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			int a;
+			int b;
+			int c;
+
+			a = 8;
+			b = 5;
+			c = 2;
+
+			Console.WriteLine("Hola, esta es la primera actividad de programacion, produce numeros aacb, bca y bcbc");
+			Console.WriteLine("a=8  b=5  c=2");
+
+			Console.WriteLine();
+			
+			Console.Write(a);
+			Console.Write(a);
+			Console.Write(c);
+			Console.Write(b);
+
+			Console.WriteLine("--------");
+
+			Console.Write(b);
+			Console.Write(c);
+			Console.Write(a);
+
+			Console.WriteLine("-------");
+
+			Console.Write(b);
+			Console.Write(c);
+			Console.Write(b);
+			Console.Write(c);
+
+			Console.ReadLine();
+		}
+	}
+}
